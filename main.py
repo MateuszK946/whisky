ea0c996b4062284eb0402292d9910dd5eea06e73
Mateusz <kochanski.mateusz94@gmail.com>
2020-12-05 10:03:29 +0100

    3 whisky działające

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from flask import Flask
 from models import Whisky, db
-from flask import render_template
+from flask import render_template, request, redirect
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///whisky.db'
 db.init_app(app)
 app.app_context().push()
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def hello_world():
-    return render_template('index.html')
+    whiskies = Whisky.query.limit(3).all()
+    return render_template('index.html', whisky=whiskies)
 
-@app.route('/<int:id>')
-def whisky(id = None):
-    return render_template('index.html', id = id)
+@app.route('/<int:id>', methods=['POST', 'GET'])
+def whisky(id):
+    whisky = Whisky.query.get_or_404(id)
+
+    if request.method == 'POST':
+        return redirect('/')
+    else:
+        return render_template('update.html', whisky=whisky)
 
 if __name__ == "__main__":
     app.run(debug=True)
+    print(Whisky.query.get_or_404(1))
     
