e6376da878d58b240929d38a22679784798fd9d6
Kamil Cybulski <kcybulski93@gmail.com>
2020-12-06 14:04:40 +0100

    Modele pod API

--- init_db.py
+++ init_db.py
@@ -1,21 +1,11 @@
 from flask import Flask
-from models import Whisky, db
+from models import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///whisky.db'
 app.app_context().push()
 
-sample_data = [
-    Whisky(name="Dalmore Cigar Malt Reserve", country="Scotland", taste="Sweet", image="static/img/dalmore-cigar-malt-reserve.jpg"),
-    Whisky(name="Glenmorangie Signet", country="Scotland", taste="Sweet", image="static/img/glenmorangie-signet.jpg"),
-    Whisky(name="Johnnie Walker Blue Label", country="Scotland", taste="Sweet", image="static/img/johnnie-walker-blue-label.jpg")
-]
-
 if __name__ == "__main__":
     db.init_app(app)
     db.create_all()
-    db.session.commit()
-
-    for data in sample_data:
-        db.session.add(data)
     db.session.commit()
--- models.py
+++ models.py
@@ -2,12 +2,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+whisky_tastes = db.Table('whisky_tastes',
+    db.Column('taste_name', db.Integer, db.ForeignKey('taste.name'), primary_key=True),
+    db.Column('whisky_id', db.Integer, db.ForeignKey('whisky.id'), primary_key=True)
+)
+
+class Taste(db.Model):
+    name = db.Column(db.String(80), primary_key=True)
+
 class Whisky(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    country = db.Column(db.String(40), nullable=False)
-    taste = db.Column(db.String(80), nullable=False)
-    image = db.Column(db.String(120), unique=True)
-    
-    def __repr__(self):
-        return f"Whisky {self.name}, {self.country}, {self.taste}"
+    region = db.Column(db.String(40), nullable=True)
+    description = db.Column(db.String(1024), nullable=True)
+    list_img_url = db.Column(db.String(256), nullable=True)
+    detail_img_url = db.Column(db.String(256), nullable=True)
+    tastes = db.relationship('Taste', secondary=whisky_tastes, lazy='subquery',
+        backref=db.backref('whiskies', lazy=True))
+
