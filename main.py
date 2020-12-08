e1e188c8b3ab4aedee1ce9398d09ac388cead48e
Mateusz <kochanski.mateusz94@gmail.com>
2020-12-08 16:17:55 +0100

    Początek sortowania

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from models import Whisky, db
+from models import Whisky, db, Taste
 from flask import render_template, request, redirect
 
 app = Flask(__name__)
@@ -7,19 +7,44 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///whisky.db'
 db.init_app(app)
 app.app_context().push()
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def hello_world():
-    whiskies = Whisky.query.limit(3).all()
+    whiskies = Whisky.query.all()
     return render_template('index.html', whisky=whiskies)
 
 @app.route('/<int:id>', methods=['POST', 'GET'])
 def whisky(id):
     whisky = Whisky.query.get_or_404(id)
-
+    tastes = []
+    for taste in whisky.tastes:
+        print(taste.name)
+        tastes.append(taste.name)
     if request.method == 'POST':
         return redirect('/')
     else:
-        return render_template('update.html', whisky=whisky)
+        return render_template('update.html', whisky=whisky, taste=(', '.join(tastes)))
+
+@app.route('/sortowanie', methods=['POST', 'GET'])
+def sortowanie():
+    whisky = Whisky.query.all()
+    tastes = []
+    for name in Taste.query.all():
+        tastes.append(name.name)
+
+    if request.method == 'POST':
+        # taste0 = request.form.get('taste0')
+        # taste1 = request.form.get('taste1')
+        # taste2 = request.form.get('taste2')
+        # print(taste0, taste1, taste2)
+        # if taste0 or taste1 or taste2 in whisky.tastes:
+        #     print(whisky.name)
+        return redirect('/')
+    else:
+        return render_template('sortowanie.html', taste=tastes) # taste=(', '.join(tastes)))
+
+@app.route('/lista', methods=['POST', 'GET'])
+def lista():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
